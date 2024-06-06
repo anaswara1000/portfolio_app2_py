7a4238250c35e83e02af4cf3c9bb742bda324e2b
anaswara <anaswara2130@gmail.com>
2024-06-06 19:06:57 +0530

    Add data source and image files

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("submit")
+    print(button)
+    if button:
+        message = message + user_email
+        ijfvdjvi
+        ufhuh message
